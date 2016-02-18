9d12b54be0b8dedf19a0f23b78aeab0d423e18d5
ktejas <krishnasalimeda@gmail.com>
2016-02-18 12:00:00 -0700

    Changed project to Third Person Controller

---
+++ Source/Shift/Shift.Build.cs
@@ -0,0 +1,11 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Shift : ModuleRules
+{
+	public Shift(TargetInfo Target)
+	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+	}
+}
---
+++ Source/Shift/Shift.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __SHIFT_H__
+#define __SHIFT_H__
+
+#include "EngineMinimal.h"
+
+#endif
